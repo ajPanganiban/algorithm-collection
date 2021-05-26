26aff35a8a93c17f26807e585db99ccf635dbd7d
ajPanganiban <arvinjaypanganban24@gmail.com>
2021-05-26 19:22:45 +0800

    feat(): create algo and helper for minm function

--- RootFinders/MINM/algo.py
+++ RootFinders/MINM/algo.py
@@ -4,35 +4,36 @@ from sympy import *
 from helper import *
 
 x = symp.Symbol('x')
-f = ((x-1)**5)*(x-3)
-f_prime = symp.diff(f)
 
-### Step 1 ###
-xk = [0,2]
-epsilon = 0.001
-k = 0
-dif = 1
+def minm(f,interval,epsilon):
+    '''Implements the Modified Interval Newton Method by Ansary and Panda.
+        This algorithm seeks to find a root of f inside a given interval.
+    :param f: function (over the symbol 'x')
+    :type f: symp function
+    :param interval: close interval cosidered
+    :type interval: list (of endpoints)
+    :param epsilon: tolerance value
+    :type epsilon: float
+    :returns: interval of length smaller than epsilon containing a root of f
+    '''
+    f_prime = symp.diff(f)
 
-### Step 2 ###
-while abs(dif) > epsilon/100:
-    f_prime_xk = function_image(f_prime,xk[0],xk[1])
+    xk = interval
+    dif = 1
 
-### Step 3 ###
-    xk_mid = (xk[0] + xk[1])/2
-    N_tilde = interval_subtraction([xk_mid,xk_mid],interval_division([f.subs(x,xk_mid),f.subs(x,xk_mid)],f_prime_xk))
-    xk_tilde = interval_intersection(xk,N_tilde)
-
-### Step 4 ###
-    f_prime_xk_tilde = function_image(f_prime,xk_tilde[0],xk_tilde[1])
-
-### Step 5 ###
-    xk_tilde_mid = (xk_tilde[0] + xk_tilde[1])/2
-    f_xk_tilde = function_image(f,xk_tilde[0],xk_tilde[1])
-    f_prime_xk_tilde = function_image(f_prime,xk_tilde[0],xk_tilde[1])
-    N_tilde_hat = interval_subtraction([xk_tilde_mid,xk_tilde_mid],interval_division([2*f.subs(x,xk_tilde_mid),2*f.subs(x,xk_tilde_mid)], \
-                                                                                    interval_addition(f_prime_xk,f_prime_xk_tilde)))
-    xk = interval_intersection(xk,N_tilde_hat)
-    if xk[1] < xk[0]:
-        xk = [xk[1], xk[0]]
-    dif = xk[1] - xk[0]
-    print(xk)
+    while abs(dif) > epsilon:
+        f_prime_xk = function_image(f_prime,xk[0],xk[1])
+        xk_mid = (xk[0] + xk[1])/2
+        N_tilde = interval_subtraction([xk_mid,xk_mid],interval_division([f.subs(x,xk_mid),f.subs(x,xk_mid)],f_prime_xk))
+        xk_tilde = interval_intersection(xk,N_tilde)
+        f_prime_xk_tilde = function_image(f_prime,xk_tilde[0],xk_tilde[1])
+        xk_tilde_mid = (xk_tilde[0] + xk_tilde[1])/2
+        f_prime_xk_tilde = function_image(f_prime,xk_tilde[0],xk_tilde[1])
+        N_tilde_hat = interval_subtraction([xk_tilde_mid,xk_tilde_mid],interval_division(
+                                                                        [2*f.subs(x,xk_tilde_mid),2*f.subs(x,xk_tilde_mid)], \
+                                                                        interval_addition(f_prime_xk,f_prime_xk_tilde)))
+        xk = interval_intersection(xk,N_tilde_hat)
+        if xk[1] < xk[0]:
+            xk = [xk[1], xk[0]]
+        dif = xk[1] - xk[0]
+    return xk
